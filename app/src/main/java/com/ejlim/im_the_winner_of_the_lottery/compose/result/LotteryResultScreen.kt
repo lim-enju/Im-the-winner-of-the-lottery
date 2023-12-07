93a8e6f6abbef7f17a2a434b7b0bbb766cd01348
limeunju <enjulim7056@gmail.com>
2023-12-07 22:03:58 +0900

    feat: 회차정보, 1등 번호를 javascript interface로 html에 전달

--- app/src/main/java/com/ejlim/im_the_winner_of_the_lottery/compose/result/LotteryResultScreen.kt
+++ app/src/main/java/com/ejlim/im_the_winner_of_the_lottery/compose/result/LotteryResultScreen.kt
@@ -1,17 +1,23 @@
 package com.ejlim.im_the_winner_of_the_lottery.compose.result
 
 import android.annotation.SuppressLint
+import android.util.Log
 import android.view.ViewGroup
+import android.webkit.ConsoleMessage
+import android.webkit.JavascriptInterface
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ejlim.im_the_winner_of_the_lottery.viewmodels.LotteryResultViewModel
+import com.google.gson.Gson
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -19,16 +25,23 @@ import com.ejlim.im_the_winner_of_the_lottery.viewmodels.LotteryResultViewModel
 fun LotteryResultScreen(
     lotteryResultViewModel: LotteryResultViewModel = hiltViewModel()
 ){
-    val url = lotteryResultViewModel.qrUrl.collectAsState(initial = null)
     Scaffold(
     ) {
-        LotteryWebView()
+        LotteryWebView(
+            lotteryResultViewModel.roundNumber,
+            lotteryResultViewModel.myLotteryNumber,
+            lotteryResultViewModel.winnerLotteryNumber
+        )
     }
 }
 
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
-fun LotteryWebView() {
+fun LotteryWebView(
+    roundNumber: Int,
+    myLotteryNumber: List<List<Int>>,
+    winnerLotteryNumber: List<Int>
+) {
     val context = LocalContext.current
     //webview instance가 한번만 생성되고 계속 재사용 됨
     AndroidView(factory = {//뷰 생성
@@ -38,9 +51,44 @@ fun LotteryWebView() {
                 ViewGroup.LayoutParams.MATCH_PARENT
             )
             webViewClient = WebViewClient()
+            webChromeClient = object : WebChromeClient() {
+                override fun onConsoleMessage(cm: ConsoleMessage): Boolean {
+                    Log.d(
+                        "WebView",
+                        "${cm.message()} -- From line ${cm.lineNumber()} of ${cm.sourceId()}"
+                    )
+                    return true
+                }
+            }
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+
+            addJavascriptInterface(LotteryNumberJavascriptInterface(
+                roundNumber,
+                myLotteryNumber,
+                winnerLotteryNumber
+            ), "LotteryAndroid")
             loadUrl("file:///android_asset/QRResult.html")
         }
-    }, update = {
-        it.loadUrl("file:///android_asset/QRResult.html")
     })
+}
+
+class LotteryNumberJavascriptInterface(
+    val roundNumber: Int,
+    val allLotteryNumber: List<List<Int>>,
+    val winnerLotteryNumber: List<Int>
+){
+    @JavascriptInterface
+    public fun getRoundNumberFromAndroid(): Int
+        = roundNumber
+
+    @JavascriptInterface
+    public fun getWinnerNumberFromAndroid(): String {
+        Log.d("EJLIM11", "getWinnerNumberFromAndroid: ${winnerLotteryNumber.map { " $it" }}")
+         return Gson().toJson(winnerLotteryNumber)
+    }
+    @JavascriptInterface
+    public fun getAllNumberFromAndroid(): String{
+        return Gson().toJson(allLotteryNumber)
+    }
 }
--- app/src/main/java/com/ejlim/im_the_winner_of_the_lottery/viewmodels/LotteryResultViewModel.kt
+++ app/src/main/java/com/ejlim/im_the_winner_of_the_lottery/viewmodels/LotteryResultViewModel.kt
@@ -4,13 +4,19 @@ import android.net.Uri
 import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.transform
+import kotlinx.coroutines.launch
 import java.net.URI
 import java.net.URL
 import javax.inject.Inject
@@ -18,37 +24,85 @@ import javax.inject.Inject
 
 @HiltViewModel
 class LotteryResultViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle
+   private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
     private val _toastMsg = MutableSharedFlow<String>()
     val toastMsg = _toastMsg.asSharedFlow()
 
-    val qrUrl =
-        savedStateHandle
-            .getStateFlow(QR_URL_SAVED_STATE_KEY, "")
-            .transform { url ->
-                Log.d("EJLIM", "LotteryResultViewModel: $url")
+    var roundNumber: Int = 0
 
-                val lotteryUrl = Uri.parse(url)
+    var myLotteryNumber: List<List<Int>> = listOf()
 
-                //string file로 변환
-                val host = kotlin.runCatching { lotteryUrl.host }.getOrNull()
-                if(host != "m.dhlottery.co.kr"){
-                    _toastMsg.emit("lottery url 형식이 아닙니다.")
-                    return@transform
-                }
+    var winnerLotteryNumber: List<Int> = listOf()
 
-                //선택한 번호
-                val query = lotteryUrl.getQueryParameter("v")
-                val queryString = query?.split("q")
+    init {
+        decodeLotteryQRUrl()
+    }
 
-                //회차정보와 번호 정보가 있는지 확인
-                if(queryString == null || queryString.size < 2) {
-                    _toastMsg.emit("정보가 올바르지 않습니다.")
-                    return@transform
-                }
-                emit(queryString)
+    private fun decodeLotteryQRUrl() {
+        val url = savedStateHandle.get<String>(QR_URL_SAVED_STATE_KEY)
+        val lotteryUrl = Uri.parse(url)
+
+        //string file로 변환
+        val host = kotlin.runCatching { lotteryUrl.host }.getOrNull()
+        if (host != "m.dhlottery.co.kr") {
+            viewModelScope.launch {
+                _toastMsg.emit("lottery url 형식이 아닙니다.")
             }
+            return
+        }
+
+        //tr정보를 제외하고 파싱
+        val query = lotteryUrl.getQueryParameter("v")?.let { vString ->
+            if (vString.length > 10)
+                vString.substring(0, vString.length - 10)
+            else null
+        }
+
+        //기준 문자 찾기
+        val standardChar = query?.first { !it.isDigit() }
+        if (standardChar == null) {
+            viewModelScope.launch {
+                _toastMsg.emit("정보가 올바르지 않습니다.")
+            }
+            return
+        }
+
+        //회차정보와 번호 정보가 있는지 확인
+        val queryString = query.split(standardChar)
+        if (queryString.size < 2) {
+            viewModelScope.launch {
+                _toastMsg.emit("정보가 올바르지 않습니다.")
+            }
+            return
+        }
+
+        //로또번호를 string 에서 int로 변환
+        val myNumber = queryString.map { numberString ->
+            numberString
+                .chunked(2)
+                .filter { number -> number != "00" }
+                .mapNotNull {
+                    it.toIntOrNull()
+                }
+        }
+
+        //회차정보
+        roundNumber = queryString[0].toIntOrNull() ?: 0
+
+        //로또 번호
+        myLotteryNumber = myNumber
+
+        //1등 번호
+        val winnerIndex = (1 until queryString.size).random()
+        winnerLotteryNumber = myNumber[winnerIndex]
+
+        Log.d("EJLIM11", "NUMBER: " +
+                "회차 ${queryString[0].toIntOrNull() ?: 0} " +
+                "번호 ${myNumber.map { " $it" }}" +
+                "1등 번호 ${myNumber[winnerIndex].map { " $it" }}"
+        )
+    }
 
     companion object {
         private const val QR_URL_SAVED_STATE_KEY = "qr_url"
